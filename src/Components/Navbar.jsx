04ac90ffd0657a2e17cac0252028d5717966a968
Ben H <55806947+benharris28@users.noreply.github.com>
2022-12-18 20:42:59 +0000

    Updated nav bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {
   Routes,
@@ -16,10 +17,11 @@ import LoginModal from "./Login/LoginModal";
 
 function NavBar() {
   const [showModal, setShowModal] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-   const handleCloseModal = () => setShowModal(false);
+  const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
-  
+
   const {
     user,
     isAuthenticated,
@@ -33,58 +35,69 @@ function NavBar() {
     });
 
   const url = '/clinics'
-  
-    return (
-      <div>
-       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Link to={`/`}>
-          <Navbar.Brand href="#home">Filling In</Navbar.Brand>
+            <Navbar.Brand href="#home">Filling In</Navbar.Brand>
           </Link>
+          {!isAuthenticated &&
+            <Nav.Link 
+              href="#features"
+              onClick={handleShowModal}
+              >Login</Nav.Link>
+          }
+          {!isAuthenticated &&
+            <Button
+              onClick={handleShowModal}>
+              Sign Up
+            </Button>
+          }
+
+  
+
+          
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Link to={`/shifts`}>
-            <Nav.Link href="#features">Shifts</Nav.Link>
-            </Link>
-            <Nav.Link href="#pricing">How it works</Nav.Link>
-           
-          </Nav>
-          <Nav>
-            <Link to={`/clinics`}>
-            <Nav.Link href="#deets">My Clinic</Nav.Link>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Link to={`/shifts`}>
+                <Nav.Link href="#features">Shifts</Nav.Link>
               </Link>
-            {!isAuthenticated &&
-            <Button
-             onClick={() => loginWithRedirect({ redirectUri: 'https://filling-in.benharris28.repl.co/clinics', signup: true })}>
-              Login as Clinic
-            </Button>
-            }
-             {!isAuthenticated &&
-            <Button
-             onClick={handleShowModal}>
-              Signup
-            </Button>
-            }
-            {isAuthenticated &&
-            <Button
-              onClick={() => logoutWithRedirect()}>
-              Logout
-            </Button>
-            }
-          </Nav>
-        </Navbar.Collapse>
+              <Nav.Link href="#pricing">How it works</Nav.Link>
+
+            </Nav>
+            <Nav>
+              <Link to={`/clinics`}>
+                <Nav.Link href="#deets">My Clinic</Nav.Link>
+              </Link>
+              {!isAuthenticated &&
+                <Button
+                  onClick={() => loginWithRedirect({ redirectUri: 'https://filling-in.benharris28.repl.co/clinics', signup: true })}>
+                  Login as Clinic
+                </Button>
+              }
+
+              {isAuthenticated &&
+                <Button
+                  onClick={() => logoutWithRedirect()}>
+                  Logout
+                </Button>
+              }
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
-        <LoginModal
+      <LoginModal
         show={showModal}
         handleClose={handleCloseModal}
         handleShow={handleShowModal}
       />
-       
-      </div>
-    )
+
+    </div>
+  )
 }
 
 export default NavBar;
