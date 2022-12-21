b5b1f27327cf81ffecb9591cab0b6e9e8f67c54a
Ben H <55806947+benharris28@users.noreply.github.com>
2022-12-21 21:44:28 +0000

    Added structure and content so that cards can be populated with credentials in credentials component

--- src/Components/ClinicDetails.jsx
+++ src/Components/ClinicDetails.jsx
@@ -1,54 +1,39 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Image, Badge, Button } from 'react-bootstrap';
 
+// Connect to context to bring in all required credentials
+// Bring in array of all credentials and IDs of each field
+// Bring in status of each credential
+// Render an "upload" button if the status is empty for new credentials which will need a file picker
+// Connect to Airtable
+// Post to Airtable when "upload is clicked"
 const ClinicDetails = () => {
+
+  const cards = [  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' },  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' },  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' },  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' },  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' },  
+    { image: 'https://res.cloudinary.com/dhkmle6ei/image/upload/v1666205828/GET_10_ykcs3x.png' }
+  ]
+
   return (
     <div>
       <h4>Clinic Details</h4>
-    <Container fluid>
+      <Container>
       <Row>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 1
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 2
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 3
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 4
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 5
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={4} lg={2}>
-          <Card>
-            <Card.Body>
-              Card 6
-            </Card.Body>
-          </Card>
-        </Col>
+        {cards.map((card, index) => (
+          <Col key={index} xs={12} md={4} lg={2}>
+            <Card>
+              <Card.Body>
+                <Image src={card.image} thumbnail />
+                <Badge variant="success">Approved</Badge>
+                <Button variant="primary">Upload</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
     </div>
